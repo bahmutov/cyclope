7209fd6b5554c168838f0903c69524d649efa720
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-09-24 08:07:34 -0400

    save page

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -7,5 +7,7 @@ it('creates accurate DOM snapshots', () => {
   cy.get('[data-cy=add-todo]')
     .type('Learn Cypress{enter}')
     .type('Write tests{enter}')
-  cy.get('[data-cy=todo]').should('have.length', 2).then(savePage('output'))
+  cy.get('[data-cy=todo]')
+    .should('have.length', 2)
+    .then(savePage('page/two-items'))
 })
