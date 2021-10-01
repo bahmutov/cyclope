667ae06939275519b561d709a3509a596cce515f
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-09-30 22:05:21 -0400

    refactor plugin file

--- plugin/index.js
+++ plugin/index.js
@@ -9,40 +9,44 @@ const { zipFolder } = require('./zip')
 
 const pipeline = promisify(stream.pipeline)
 
+async function makeFolder(path) {
+  console.log('making folder %s', path)
+  await mkdirp(path, { recursive: true })
+  return path
+}
+
+async function saveResource({ outputFolder, fullUrl, srcAttribute }) {
+  console.log('saving %s -> %s', fullUrl, srcAttribute)
+  if (!fullUrl) {
+    throw new Error('Missing fullUrl')
+  }
+
+  const savePath = path.join(outputFolder, srcAttribute)
+  const folder = path.dirname(savePath)
+  await mkdirp(folder, { recursive: true })
+
+  await pipeline(got.stream(fullUrl), fs.createWriteStream(savePath))
+
+  return null
+}
+
+async function zipFolder({ folder, zipFile }) {
+  console.log('zipping %s to %s', folder, zipFile)
+
+  await del(zipFile)
+
+  await zipFolder(folder, zipFile)
+
+  await del(folder)
+
+  return zipFile
+}
+
 function initCyclope(on, config) {
   on('task', {
-    async makeFolder(path) {
-      console.log('making folder %s', path)
-      await mkdirp(path, { recursive: true })
-      return path
-    },
-
-    async saveResource({ outputFolder, fullUrl, srcAttribute }) {
-      console.log('saving %s -> %s', fullUrl, srcAttribute)
-      if (!fullUrl) {
-        throw new Error('Missing fullUrl')
-      }
-
-      const savePath = path.join(outputFolder, srcAttribute)
-      const folder = path.dirname(savePath)
-      await mkdirp(folder, { recursive: true })
-
-      await pipeline(got.stream(fullUrl), fs.createWriteStream(savePath))
-
-      return null
-    },
-
-    async zipFolder({ folder, zipFile }) {
-      console.log('zipping %s to %s', folder, zipFile)
-
-      await del(zipFile)
-
-      await zipFolder(folder, zipFile)
-
-      await del(folder)
-
-      return zipFile
-    },
+    makeFolder,
+    saveResource,
+    zipFolder,
   })
 }
 
