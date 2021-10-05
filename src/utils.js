31afbfe116ebfb96cf835271648ef88a07983706
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-10-05 12:11:28 -0400

    fix: require unique default

--- src/utils.js
+++ src/utils.js
@@ -2,7 +2,12 @@
 /// <reference types="cypress" />
 
 // https://github.com/ericclemmons/unique-selector
-const unique = require('unique-selector')
+const unique = require('unique-selector').default
+
+if (typeof unique !== 'function') {
+  console.error('unique is', unique)
+  throw new Error('unique-selector is not a function')
+}
 
 const jUnique = ($el) => unique($el[0])
 
