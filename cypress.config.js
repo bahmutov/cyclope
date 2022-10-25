dea498f6326fb39ba3c20b2aff69b1471b875153
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-10-25 07:35:59 -0400

    config file tweaks

--- cypress.config.js
+++ cypress.config.js
@@ -3,19 +3,18 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   fixturesFolder: false,
   viewportWidth: 500,
-  experimentalSessionSupport: true,
   env: {
     cyclope: {
       skipUploadWithoutUrl: true,
     },
   },
   e2e: {
+    experimentalSessionAndOrigin: true,
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
       return require('./cypress/plugins/index.js')(on, config)
     },
     baseUrl: 'http://localhost:3777',
-    excludeSpecPattern: ['utils.js', '*.html'],
   },
 })
