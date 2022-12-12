a609208698f3cd17373f270af64d9dc00f3a6ab6
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-12-12 15:08:19 -0500

    test absolute import

--- cypress/e2e/import.cy.js
+++ cypress/e2e/import.cy.js
@@ -6,8 +6,15 @@ it('handles import directive', () => {
   cy.get('.imported')
     .should('have.css', 'border')
     .should('include', 'rgb(255, 0, 0)')
+  cy.get('.imported2')
+    .should('have.css', 'border')
+    .should('include', 'rgb(0, 128, 0)')
+
+  cy.log('**saving the page**')
   cy.savePage('import')
   cy.readFile('import/index.html').then((html) => {
-    expect(html, 'html').to.include("@import url('./import/app.css')")
+    expect(html, 'html')
+      .to.include("@import url('./import/app.css')")
+      .and.to.include("@import url('./import/more-styles.css')")
   })
 })
