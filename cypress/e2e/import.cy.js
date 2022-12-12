77b0174fc32448fcb9562e99692bb0d1f05786c6
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-12-12 15:34:49 -0500

    fix: preserve 3rd party domain imports

--- cypress/e2e/import.cy.js
+++ cypress/e2e/import.cy.js
@@ -2,13 +2,32 @@
 
 // https://github.com/bahmutov/cyclope/issues/83
 it('handles import directive', () => {
+  // the page includes CSS import from 3rd party domain
+  cy.intercept(
+    'GET',
+    'https://other-domain.acme/style.css',
+    `
+      .imported3 {
+        border: 2px solid blue;
+      }
+    `,
+  ).as('css')
+
   cy.visit('/import')
+  cy.wait('@css')
+
+  // relative CSS import
   cy.get('.imported')
     .should('have.css', 'border')
     .should('include', 'rgb(255, 0, 0)')
+  // absolute CSS import
   cy.get('.imported2')
     .should('have.css', 'border')
     .should('include', 'rgb(0, 128, 0)')
+  // 3rd-party domain CSS import
+  cy.get('.imported3')
+    .should('have.css', 'border')
+    .should('include', 'rgb(0, 0, 255)')
 
   cy.log('**saving the page**')
   cy.savePage('import')
@@ -16,5 +35,6 @@ it('handles import directive', () => {
     expect(html, 'html')
       .to.include("@import url('./import/app.css')")
       .and.to.include("@import url('./import/more-styles.css')")
+      .and.to.not.include('undefined')
   })
 })
