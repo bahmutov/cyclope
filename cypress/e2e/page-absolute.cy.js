fc699076876f58f0c00a9cd0627555a29d5adf79
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-05-08 16:44:56 -0400

    fix: save image srcset

--- cypress/e2e/page-absolute.cy.js
+++ cypress/e2e/page-absolute.cy.js
@@ -4,14 +4,33 @@
 // the second test loads the saved static page
 // and checks if the image loads correctly
 
-it('handles absolute to relative resources', () => {
-  cy.visit('/page-absolute')
-  cy.get('img', { timeout: 0 }).should('have.prop', 'naturalWidth', 1440)
-  cy.savePage('page-absolute')
-})
+describe('image edge cases', { viewportWidth: 1500 }, () => {
+  it('handles absolute to relative resources', () => {
+    cy.visit('/page-absolute')
+    // normal image
+    cy.get('img#image1', { timeout: 0 }).should(
+      'have.prop',
+      'naturalWidth',
+      1440,
+    )
+    // image with srcset attribute
+    cy.get('img#image2', { timeout: 0 }).should('have.prop', 'naturalWidth', 40)
+    cy.savePage('page-absolute')
+  })
 
-// SKIP https://github.com/bahmutov/cyclope/issues/95
-it('loads the saved static page', { baseUrl: null }, () => {
-  cy.visit('page-absolute/index.html')
-  cy.get('img', { timeout: 0 }).should('have.prop', 'naturalWidth', 1440)
+  // these tests assume the previous test was successful
+
+  it('loads the saved static page', { baseUrl: null }, () => {
+    cy.visit('page-absolute/index.html')
+    cy.get('img#image1', { timeout: 0 }).should(
+      'have.prop',
+      'naturalWidth',
+      1440,
+    )
+  })
+
+  it('loads the saved srcset image', { baseUrl: null }, () => {
+    cy.visit('page-absolute/index.html')
+    cy.get('img#image2', { timeout: 0 }).should('have.prop', 'naturalWidth', 40)
+  })
 })
