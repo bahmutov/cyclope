c1c7bd8e1f2e54d38b83221ce3247ef2ecd17a03
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-06-01 14:31:55 -0400

    fix: remove > and < when making folders (#107)

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -7,3 +7,14 @@ it('creates accurate DOM snapshots', () => {
   cy.get('[data-cy=todo]').should('have.length', 2)
   cy.savePage('page/two-items')
 })
+
+it('test has special characters like >', () => {
+  cy.visit('/')
+  cy.get('[data-cy=add-todo]')
+    .type('Learn Cypress{enter}')
+    .type('Write tests{enter}')
+  cy.get('[data-cy=todo]').should('have.length', 2)
+  cy.savePage('page/name with > character')
+  // problematic character was removed
+  cy.readFile('page/name with - character/index.html')
+})
